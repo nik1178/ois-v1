a8d5dd8cec472f163600f87576f9106c684f4349
Matej <matejgolob2003@gmail.com>
2023-02-21 12:06:32 +0100

    dodaj izpis nadmorske in gravitacijskega pospeška #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,8 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+
+    public static void izpis(double nadVisina, double gravPosp) {
+        System.out.println("nadmorska visina: "+nadVisina + " gravitacijski pospešek: " + gravPosp);
+    }
 }
