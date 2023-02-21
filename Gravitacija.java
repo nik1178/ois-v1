aee6ca2fd76ef9194db107eaa409c2a786a6b31e
tomazcerne <tomaz.cerne03@gmail.com>
2023-02-21 12:06:16 +0100

    dodaj izračun pospeška #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,14 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+
+    public static double izracun(double nmv) {
+
+        final double C = 6.674 * Math.pow(10, -11);
+        final double M = 5.972 * Math.pow(10, 24);
+        final double R = 6.371 * Math.pow(10, 6);
+
+        double pospesek = (C*M) / ((R+nmv) * (R+nmv));
+        return pospesek;
+    }
 }
